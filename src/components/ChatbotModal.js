40861d6dda70bbeec66086120c2c01f073574dc2
Triane Peart <TrianePeart@gmail.com>
2023-06-12 15:50:49 -0400

    Chatbot

--- src/components/ChatbotModal.js
+++ src/components/ChatbotModal.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import '../style/ChatbotModal.css';
-import { Link } from 'react-router-dom';
 
 const ChatbotModal = () => {
   const [showModal, setShowModal] = useState(false);
