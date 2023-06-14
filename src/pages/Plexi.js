33ed003a9e8cab938186b78b88d9b6e89a38a090
Triane Peart <TrianePeart@gmail.com>
2023-06-13 21:55:55 -0400

    updates to plexi

--- src/pages/Plexi.js
+++ src/pages/Plexi.js
@@ -1,11 +1,88 @@
 import React from 'react';
 import PlexiBrain from '../components/PlexiBrain';
+import '../style/Plexi.scss'
 
-//FEATURE NOT WORKING 
 function Plexi() {
   return(
-<div>
-  <PlexiBrain/>
+<div className='plexi'>
+<div class="hud">
+  <div class="blue">
+    <div class="notches">
+      <div class="notch notch1"></div>
+      <div class="notch notch2"></div>
+      <div class="notch notch3"></div>
+      <div class="notch notch4"></div>
+      <div class="notch notch5"></div>
+      <div class="notch notch6"></div>
+      <div class="notch notch7"></div>
+      <div class="notch notch8"></div>
+      <div class="notch notch9"></div>
+      <div class="notch notch10"></div>
+      <div class="notch notch11"></div>
+      <div class="notch notch12"></div>
+    </div>
+    <div class="circle outer1"></div>
+    <div class="circle outer2"></div>
+    <div class="circle outer3"></div>
+    <div class="circle inner1"></div>
+    <div class="circle inner2"></div>
+    <div class="topnotches">
+      <div class="notch notch1"></div>
+      <div class="notch notch2"></div>
+      <div class="notch notch3"></div>
+      <div class="notch notch4"></div>
+      <div class="notch notch5"></div>
+      <div class="notch notch6"></div>
+      <div class="notch notch7"></div>
+      <div class="notch notch8"></div>
+      <div class="notch notch9"></div>
+      <div class="notch notch10"></div>
+      <div class="notch notch11"></div>
+      <div class="notch notch12"></div>
+    </div>
+    <div class="bottomnotches">
+      <div class="notch notch1"></div>
+      <div class="notch notch2"></div>
+      <div class="notch notch3"></div>
+      <div class="notch notch4"></div>
+      <div class="notch notch5"></div>
+      <div class="notch notch6"></div>
+      <div class="notch notch7"></div>
+      <div class="notch notch8"></div>
+      <div class="notch notch9"></div>
+      <div class="notch notch10"></div>
+      <div class="notch notch11"></div>
+      <div class="notch notch12"></div>
+    </div>
+    <div class="circle inner3"></div>
+    <div class="center">
+      <div class="top left"></div>
+      <div class="top right"></div>
+      <div class="bottom left"></div>
+      <div class="bottom right"></div>
+    </div>
+  </div>
+  <div class="pink">
+    <div class="circle inner1"></div>
+    <div class="notches">
+      <div class="notch notch1"></div>
+      <div class="notch notch2"></div>
+      <div class="notch notch3"></div>
+      <div class="notch notch4"></div>
+      <div class="notch notch5"></div>
+      <div class="notch notch6"></div>
+      <div class="notch notch7"></div>
+      <div class="notch notch8"></div>
+      <div class="notch notch9"></div>
+      <div class="notch notch10"></div>
+      <div class="notch notch11"></div>
+      <div class="notch notch12"></div>
+    </div>
+    <PlexiBrain/>
+    <div class="circle inner2"></div>
+  </div>
+
+</div>
 </div>
 )
 }
