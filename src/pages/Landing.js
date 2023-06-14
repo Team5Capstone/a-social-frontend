6b03756dd7f0615d372c4c70b32b3c5c57534b01
Triane Peart <TrianePeart@gmail.com>
2023-06-13 21:56:16 -0400

    updates to landing/signup

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Login from '../components/Login'
 import '../style/Landing.css';
+import logo from '../assets/Background.png'
 
 function Landing() {
     return (
-        <div>
-             <Login />
+        <div className='box-container'>
+           <div className="logo">
+                <img src={logo} alt="Background Logo" />
+            </div>
+            <Login />
         </div>
     );
 }
