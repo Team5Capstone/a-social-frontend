c33a0651548d1f47d0f1dece86930835c701600e
NRiddick113 <NatashaRiddick@pursuit.org>
2023-06-20 10:15:38 -0400

    finish forum and create forum

--- src/components/CreateForum.js
+++ src/components/CreateForum.js
@@ -1,27 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../style/CreateForum.css'
+import '../style/CreateForum.css';
 
 const API = process.env.REACT_APP_API_URL;
-
-const categories = [
-  'Venting and Support',
-  'Accessibility',
-  'Vibe Check',
-  'Family',
-  'Hobbies',
-  'General Chat',
-];
 
 function CreateForum() {
   const navigate = useNavigate();
   const [forumTitle, setForumTitle] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
   const [forumDescription, setForumDescription] = useState('');
   const [forumPost, setForumPost] = useState('');
   const [selectedCategories, setSelectedCategories] = useState([]);
-  const [tags, setTags] = useState([]);
+  const [selectedBadge, setSelectedBadge] = useState(null);
+  const [badges, setBadges] = useState([]);
   const userId = Number(localStorage.getItem('a-social'));
+
+  useEffect(() => {
+    axios
+      .get(`${API}/categories`)
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    axios
+      .get(`${API}/badges`)
+      .then((res) => {
+        setBadges(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,20 +45,22 @@ function CreateForum() {
       forum_description: forumDescription,
       forum_posts: forumPost,
       forum_categories: selectedCategories,
-      forum_tags: tags.split(',').map((tag) => tag.trim()),
       user_id: userId,
+      badge_id: selectedBadge
     };
-    
 
     axios
       .post(`${API}/forums`, newForum)
       .then((res) => {
-        // Handle success or redirect to the created forum
-        navigate('/forums'); // Redirect to the forums page after successful forum creation
+        navigate('/forums');
       })
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
   };
 
   const handleCategoryToggle = (category) => {
@@ -78,7 +94,7 @@ function CreateForum() {
           />
         </div>
         <div>
-          <label htmlFor="forum-post">Forum post:</label>
+          <label htmlFor="forum-post">Forum Post:</label>
           <textarea
             id="forum-post"
             value={forumPost}
@@ -86,34 +102,34 @@ function CreateForum() {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="tags">Tags:</label>
-          <input
-            type="text"
-            id="tags"
-            value={tags}
-            onChange={(e) => setTags(e.target.value)}
-            placeholder="Enter tags (comma-separated)"
-          />
+          <label htmlFor="category">Category:</label>
+          <select name="category" id="category" value={selectedCategory} onChange={handleCategoryChange}>
+            <option value="">Select a category</option>
+            {categories.map((category) => (
+              <option key={category.id} value={category.category_name}>
+                {category.category_name}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="form-group">
-          <label htmlFor="categories">Categories:</label>
-          {categories.map((category) => (
-            <div key={category} className="category-checkbox">
-              <input
-                type="checkbox"
-                id={`category-${category}`}
-                value={category}
-                checked={selectedCategories.includes(category)}
-                onChange={() => handleCategoryToggle(category)}
-              />
-              <label htmlFor={`category-${category}`}>{category}</label>
-            </div>
-          ))}
+          <label htmlFor="badges">Badges:</label>
+          <select name="badges" id="badges">
+            <option value="">Select a badge</option>
+            {badges.map((badge) => (
+              <option key={badge.id} value={badge.badge_type}>
+                {badge.badge_type}
+              </option>
+            ))}
+          </select>
         </div>
-        <button className='sub' type="submit">Create Forum</button>
+        <button className="sub" type="submit">
+          Create Forum
+        </button>
       </form>
     </div>
   );
 }
 
 export default CreateForum;
+
--- src/components/Forum.js
+++ src/components/Forum.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
-import "../style/Forum.css"; 
+import '../style/Forum.css';
 
 const API = process.env.REACT_APP_API_URL;
 
-function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
+function Forum() {
   const [forums, setForums] = useState([]);
-  const navigate = useNavigate(); //this isn't being used 
   const [users, setUsers] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('All');
+  const [categories, setCategories] = useState([]);
   const [sortBy, setSortBy] = useState('Latest');
+  const [badges, setBadges] = useState([]);
+  const [badgeName, setBadgeName] = useState('');
   const [showBackToTop, setShowBackToTop] = useState(false);
   const [userId, setUserId] = useState('');
+
 
   const findUserById = (userId) => {
     return users.find((user) => user.id === userId);
@@ -42,7 +45,6 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
       setShowBackToTop(false);
     }
   };
-
   useEffect(() => {
     axios
       .get(`${API}/forums`)
@@ -58,6 +60,20 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
       })
       .catch((err) => console.log(err));
 
+    axios
+      .get(`${API}/categories`)
+      .then((res) => {
+        setCategories(res.data);
+      })
+      .catch((err) => console.log(err));
+
+    axios
+      .get(`${API}/badges`)
+      .then((res) => {
+        setBadges(res.data);
+      })
+      .catch((err) => console.log(err));
+      
     const storedUserId = localStorage.getItem('user_id');
     if (storedUserId) {
       setUserId(storedUserId);
@@ -70,14 +86,11 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
     };
   }, []);
 
-  const categories = [
-    'Venting and Support',
-    'Accessibility',
-    'Vibe Check',
-    'Family',
-    'Hobbies',
-    'General Chat',
-  ];
+
+
+
+  console.log(forums, badges)
+
 
   const changeCategory = (e) => {
     const selectedValue = e.target.value;
@@ -101,7 +114,7 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
   let filteredForums = [...forums];
 
   if (selectedCategory !== 'All') {
-    filteredForums = forums.filter((forum) => forum.category === selectedCategory);
+    filteredForums = forums.filter((forum) => forum.category_id === parseInt(selectedCategory));
   }
 
   if (sortBy === 'Oldest') {
@@ -114,14 +127,18 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
     <div className="forum-container">
       <h1 className="forum-title">Forum</h1>
       <div className="create">
-        <a href="/forums/new" className="button">Create New Forum</a>
+        <Link to="/forums/new" className="button">
+          Create New Forum
+        </Link>
       </div>
       <div className="filter-options">
         <label htmlFor="category">Category:</label>
         <select name="category" id="category" value={selectedCategory} onChange={changeCategory}>
           <option value="All">All</option>
           {categories.map((category) => (
-            <option key={category} value={category}>{category}</option>
+            <option key={category.id} value={category.id}>
+              {category.category_name}
+            </option>
           ))}
         </select>
         <label htmlFor="sort">Sort By:</label>
@@ -133,20 +150,26 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
       <ul className="post-list">
         {filteredForums.map((forum) => {
           const user = findUserById(forum.user_id);
-          const isCurrentUser = user && user.id === userId; //This isn't being used
-
+          const isCurrentUser = user && user.id === userId;
           const tags = forum.forum_tags || [];
+
+          // Find the category object based on the category_id
+          const category = categories.find((category) => category.id === forum.category_id);
+          const categoryName = category ? category.category_name : '';
+          const badge = badges.find((badge) => badge.id === forum.badge_id);
+          const badgeName = badge ? badge.badge_type : '';
 
           return (
             <li className="post" key={forum.id}>
               <div className="post-details">
-              <Link to={`/forums/${forum.id}`} style={{ textDecoration: 'none', color: '#fff' }}><h1>{forum.forum_description}</h1></Link>
-                <p>Category: {forum.category}</p> 
-                <p>Topics: {forum.forum_topics}</p>
-                <p>Created At: {formatDate(forum.forum_created_at)} {formatTime(forum.forum_created_at)}</p>
-                {tags.length > 0 && (
-                  <p>Tags: {tags.join(', ')}</p>
-                )}
+                <Link to={`/forums/${forum.id}`}>
+                  <h1>{forum.forum_description}</h1>
+                </Link>
+                <p>Category: {categoryName}</p>
+                <span className={`label label-${badgeName}`}>{badgeName}</span>
+                <p>
+                  Created At: {formatDate(forum.forum_created_at)} {formatTime(forum.forum_created_at)}
+                </p>
               </div>
               <div className="post-creator">
                 <h2>Created by: {user ? user.username : ''}</h2>
@@ -155,10 +178,7 @@ function Forum({ setOtherUserId })  {// SetOtherUserID isn't being used
           );
         })}
       </ul>
-      <button
-        className={`back-to-top ${showBackToTop ? 'show' : ''}`}
-        onClick={scrollToTop}
-      >
+      <button className={`back-to-top ${showBackToTop ? 'show' : ''}`} onClick={scrollToTop}>
         &uarr;
       </button>
     </div>
